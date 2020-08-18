0f506795eab316d860581505c3552cc25114a281
Michael Passade <michael.passade@codeimmersives.com>
2020-08-17 20:16:12 -0400

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+const router = require('./routes/routes')
+const port = process.env.PORT
+
+require('dotenv').config()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+// app.use((req, res, next) => {
+//     res.locals.user = req.user
+//     next()
+// })
+app.use('/', router)
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB Connected')
+}).catch(err => {
+    console.log(`MongoDB Error: ${err}`)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
